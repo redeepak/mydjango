8e99e8e8da6a30907afcd1c0db71caa1cc4dba2d
Deepak Tiwari <tdeepaktiwari92@gmail.com>
2022-02-18 17:09:07 +0530

    telusko 21 lectures completed

--- templateProject/testApp/models.py
+++ templateProject/testApp/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from PIL import Image
 
 # Create your models here.
-class Destination:
-    id : int
-    name : str
-    img : str
-    desc: str
-    price: int
+class Destination(models.Model):
+    
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+    
--- templateProject/testApp/views.py
+++ templateProject/testApp/views.py
@@ -1,4 +1,5 @@
 import datetime
+from operator import truediv
 
 from django.shortcuts import render
 from .models import Destination
@@ -10,16 +11,21 @@ def index(request):
     my_dict={'insert_date':date} 
     dest1 = Destination()
     dest1.name = "Mumbai"
+    dest1.img = 'products/products-1.jpg'
     dest1.price = 500
     dest1.desc = "the city that never sleeps"
+    dest1.offer = False
     dest2 = Destination()
     dest2.name = "Delhi"
+    dest2.img = 'products/products-2.jpg'
     dest2.price = 600
     dest2.desc = "the city that never wake up"
+    dest2.offer = True
     dest3 = Destination()
     dest3.name = "GOA"
+    dest3.img = 'products/products-3.jpg'
     dest3.price = 700
     dest3.desc = "the city that always party"
-
+    dest3.offer = False
     dests = [dest1, dest2, dest3]
     return render(request,'testApp/index.html', context={'dests': dests}) 
