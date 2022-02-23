d77994cef2864736691fc2b99ed526223c170795
Deepak Tiwari <tdeepaktiwari92@gmail.com>
2022-02-23 16:44:17 +0530

    commit to test on linux machine

---
+++ calc/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request, 'calc/home.html', {'name': 'NAVIN'})
+
+def add(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    res = val1 + val2
+    return render(request, 'calc/result.html', {'result': res})
